842e5fb73e3da97a6cd84420fd046c453b2b7214
K D <kreutz@acm.org>
2020-11-26 18:11:27 -0300

    last updates done at Thu Nov 26 18:11:26 -03 2020

---
+++ python/protocolo_acs_hmac.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+
+#                   ** NOTA **
+
+# Implementacao simplificada do protocolo ACS (com HMAC).
+# Esta implementacao eh meramente para fins didaticos.
+# Serve para ilustrar a complexidade entre a especificacao,
+# verificacao e implementacao. A implementacacao deveria,
+# tambem, passar por uma verificacao de codigo.
+# 
+# Implementacao testada no Debian 10 e Python 3.7.3.
+#
+# Nao ha nenhum tipo de garantia com relacao a este codigo.
+
+#                   ** NOTA **
+
+import sys
+import socket
+import random
+import time
+import hmac
+import hashlib
+import secrets
+from Crypto.Cipher import AES
+import base64, os
+
+char_de_padding = "{"
+
+def uso():
+    print("Uso: " + sys.argv[0] + " <ip> <porta> <servidor|cliente> <Alice|Bob> <chave_secreta> <n_trocas_de_chave>")
+    sys.exit(-1)
+
+def gera_nonce_32hex():
+    return secrets.token_hex(16)
+
+def aes_decifrar(chave_secreta, mensagem_cifrada):
+    global char_de_padding
+    cipher = AES.new(chave_secreta)
+    dado_decifrado = cipher.decrypt(mensagem_cifrada)
+    dado_decifrado = dado_decifrado.decode()
+    dado_sem_padding = dado_decifrado.rstrip(char_de_padding)
+    return dado_sem_padding
+
+def aes_cifrar(chave_secreta, mensagem):
+    global char_de_padding
+    cipher = AES.new(chave_secreta)
+    mensagem_com_padding = mensagem + (char_de_padding * ((16-len(mensagem)) % 16))
+    mensagem_cifrada = cipher.encrypt(mensagem_com_padding)
+    return mensagem_cifrada
+
+def aes_atualiza_chave(chave_secreta):
+    if len(chave_secreta) < 16:
+        chave_secreta = chave_secreta + (char_de_padding * ((16-len(chave_secreta)) % 16))
+    else:
+        chave_secreta = chave_secreta[:16]
+    return chave_secreta
+
+def hash_nova_chave(chave_secreta, nonce):
+    gerador_hash = hashlib.new('sha256')
+    gerador_hash.update(bytes(chave_secreta+nonce,encoding='utf-8'));
+    chave_secreta = aes_atualiza_chave(gerador_hash.hexdigest())
+    return chave_secreta
+
+def servidor(ip, porta, entidade, chave_secreta, n_trocas_de_chave):
+
+    sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
+    
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    endereco_servidor = (ip, porta)
+    
+    print ("inciando servidor no IP %s e PORTA %s" % endereco_servidor)
+    
+    sock.bind(endereco_servidor)
+    
+    sock.listen(5)
+    
+    print ("aguardando conexao do cliente ... ")
+    cliente, endereco = sock.accept()
+    print (" cliente %s endereco %s conectado " % (cliente, endereco))
+
+    for i in range(n_trocas_de_chave):
+
+        mensagem = cliente.recv(109)
+        mensagem = mensagem.decode('utf-8')
+
+        if mensagem:
+            print ("[" + entidade + ":" + str(i) + "] mensagem: " + mensagem)
+            mensagem_cifrada, hmac_da_msg = mensagem.split()
+            print ("[" + entidade + ":" + str(i) + "] chave: " + chave_secreta)
+            gerador_hmac = hmac.new(
+                bytes(chave_secreta, encoding='utf-8'),
+                bytes(mensagem_cifrada, encoding='utf-8'),
+                hashlib.sha256
+                )
+            hmac_gerado = gerador_hmac.hexdigest()
+            if (hmac_da_msg == hmac_gerado):
+                mensagem_cifrada = base64.b64decode(mensagem_cifrada)
+                nonce = aes_decifrar(chave_secreta, mensagem_cifrada)
+                print ("[" + entidade + ":" + str(i) + "] nonce: " + nonce)
+                chave_secreta = hash_nova_chave(chave_secreta, nonce)
+                chave_secreta = aes_atualiza_chave(chave_secreta)
+            else:
+                print ("[" + entidade + ":" + str(i) + "] ERRO no verificacao do HMAC")
+        else:
+            print ("[" + entidade + ":" + str(i) + "] ERRO no recebimento da mensagem")
+        print (" ")
+
+    sock.close()
+     
+def cliente(ip, porta, entidade, chave_secreta, n_trocas_de_chave):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    endereco_servidor = (ip, porta)
+
+    print("conectando ao servidor no %s porta %s" % endereco_servidor)
+
+    sock.connect(endereco_servidor)
+    
+    for i in range(n_trocas_de_chave):
+        nonce = gera_nonce_32hex()
+
+        print ("[" + entidade + ":" + str(i) + "] chave: " + chave_secreta)
+        print ("[" + entidade + ":" + str(i) + "] nonce: " + nonce)
+
+        mensagem = aes_cifrar(chave_secreta, nonce)
+        mensagem = base64.b64encode(mensagem)
+        hmac_sha256 = hmac.new(
+                bytes(chave_secreta, encoding='utf-8'),
+                mensagem,
+                hashlib.sha256
+        )
+        hmac_mensagem = hmac_sha256.hexdigest()
+
+        print ("[" + entidade + ":" + str(i) + "] hmac: " + hmac_mensagem)
+
+        sock.sendall(mensagem + b' ' + hmac_mensagem.encode('utf-8'))
+        chave_secreta = hash_nova_chave(chave_secreta, nonce)
+        chave_secreta = aes_atualiza_chave(chave_secreta)
+
+        print (" ")
+
+    sock.close()
+
+def main():
+
+    if len(sys.argv) < 7:
+        uso()
+
+    chave_secreta = aes_atualiza_chave(sys.argv[5])
+
+    if sys.argv[3] == "servidor":
+        servidor(
+                sys.argv[1], # ip
+                int(sys.argv[2]), # porta
+                sys.argv[4], # entidade (Alice|Bob)
+                chave_secreta, # chave secreta
+                int(sys.argv[6])  # numero de trocas de chave
+                )
+    elif sys.argv[3] == "cliente":
+        cliente(
+                sys.argv[1], # ip
+                int(sys.argv[2]), # porta
+                sys.argv[4], # entidade (Alice|Bob)
+                chave_secreta, # chave secreta
+                int(sys.argv[6])  # numero de trocas de chave
+                )
+    else:
+        uso()
+
+if __name__ == "__main__":
+    main()
